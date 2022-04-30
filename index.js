a1107cb7e5332fcede9cd8e2e7f3d080cfd76233
sivaprasad-131413 <79079939+sivaprasad-131413@users.noreply.github.com>
2022-04-30 22:21:53 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+//Oue.1--Create a button , on click of which you should be able to hide a paragraph text, create another button onclick of which this paragraph should be visible
+
+$("button").click(function(){
+    $("p").hide();
+})
+$(".show").click(function(){
+    $("p").show();
+});
+
+//Que.2--Create an ajax call, by using this api "https://jsonplaceholder.typicode.com/todos/12" and print the title as the heading of the pages.
+$(".Head").click(function(){
+    $.ajax({
+        url : "https://jsonplaceholder.typicode.com/todos/12",
+        type : "GET",
+        success : function(data){
+            console.log(data);
+            document.getElementById("head").innerText=data.title;
+        }
+
+    })
+})
